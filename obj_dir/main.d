cf17986ea9716c2702fa24deecf10813f52da618
zeigous <153451177+thatonepcguy@users.noreply.github.com>
2025-03-30 18:28:48 -0600

    Delete obj_dir directory

--- obj_dir/main.d
+++
@@ -1,9 +0,0 @@
-main.o: ../main.cpp /usr/share/verilator/include/verilated.h \
- /usr/share/verilator/include/verilatedos.h \
- /usr/share/verilator/include/verilated_config.h \
- /usr/share/verilator/include/verilated_types.h \
- /usr/share/verilator/include/verilated_funcs.h \
- /usr/share/verilator/include/verilated_vcd_c.h \
- /usr/share/verilator/include/verilated.h \
- /usr/share/verilator/include/verilated_trace.h \
- /usr/share/verilator/include/verilated_trace_defs.h ../obj_dir/Vmain.h
